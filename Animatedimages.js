adecce2fe10ce6f60cb47404515a9272de9d95fa
sharon <rpgfxwq@gmail.com>
2023-04-02 09:39:08 +0300

    Add files via upload

---
+++ Animatedimages.js
@@ -0,0 +1,10 @@
+﻿const imageContainer = document.getElementById('image-container');
+let xPos = 0;
+
+function moveImages() {
+    xPos += 10;
+    imageContainer.style.transform = `translateX(${xPos}px)`;
+    requestAnimationFrame(moveImages);
+}
+
+moveImages();
